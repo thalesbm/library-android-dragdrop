92d41bd32f63bf8eeae3d77f6e03f06d56411219
Thales Marega <thales.bm92@gmail.com>
2021-09-18 13:23:14 -0300

    organizado projeto de exemplo

--- app/src/main/java/bm/it/mobile/app/MainActivity.kt
+++ app/src/main/java/bm/it/mobile/app/MainActivity.kt
@@ -1,7 +1,9 @@
 package bm.it.mobile.app
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.LinearLayoutCompat
@@ -14,49 +16,66 @@ class MainActivity : AppCompatActivity(), OnViewSelection {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val draggableViewMain = DraggableView(this)
-        draggableViewMain.setDestViewGroup(getDestinations())
-        draggableViewMain.addViews(getViews())
+        xml()
+        code()
     }
 
-    private fun getDestinations(): MutableList<View> {
-        val destination: LinearLayoutCompat = findViewById(R.id.destination)
-        val destination1: LinearLayoutCompat = findViewById(R.id.destination1)
-        val destination2: LinearLayoutCompat = findViewById(R.id.destination2)
+    private fun xml() {
+        val destination1: LinearLayoutCompat = findViewById(R.id.destination)
+        val destination2: LinearLayoutCompat = findViewById(R.id.destination1)
+        val destination3: LinearLayoutCompat = findViewById(R.id.destination2)
+        val img1: AppCompatImageView = findViewById(R.id.imgFacebook)
+        val img2: AppCompatImageView = findViewById(R.id.imgGoogle)
+        val img3: AppCompatImageView = findViewById(R.id.imgLinkedIn)
 
         val destinations = mutableListOf<View>()
-        destinations.add(destination)
         destinations.add(destination1)
         destinations.add(destination2)
-
-        return destinations
-    }
-
-    private fun getViews(): MutableList<View> {
-        val imgFacebook: AppCompatImageView = findViewById(R.id.imgFacebook)
-        val imgGoogle: AppCompatImageView = findViewById(R.id.imgGoogle)
-        val imgLinkedIn: AppCompatImageView = findViewById(R.id.imgLinkedIn)
+        destinations.add(destination3)
 
         val views = mutableListOf<View>()
-        views.add(imgFacebook)
-        views.add(imgGoogle)
-        views.add(imgLinkedIn)
+        views.add(img1)
+        views.add(img2)
+        views.add(img3)
 
-        return views
+        val draggableView = DraggableView(this)
+        draggableView.setDestViewGroup(destinations)
+        draggableView.addViews(views)
+    }
+
+    private fun code() {
+        val itemsView: LinearLayoutCompat = findViewById(R.id.view_items)
+        val destinationsViews: LinearLayoutCompat = findViewById(R.id.view_destination)
+
+        val customParams = (LinearLayoutCompat.LayoutParams(200, 200))
+        customParams.setMargins(10, 10, 10, 10)
+
+        val views = mutableListOf<View>()
+        for (i in 0..2) {
+            val image = AppCompatImageView(this)
+            image.setBackgroundColor(Color.BLACK)
+            image.layoutParams = customParams
+
+            itemsView.addView(image)
+            views.add(image)
+        }
+
+        val destinations = mutableListOf<View>()
+        for (i in 0..2) {
+            val image = LinearLayoutCompat(this)
+            image.setBackgroundColor(Color.YELLOW)
+            image.layoutParams = customParams
+
+            destinationsViews.addView(image)
+            destinations.add(image)
+        }
+
+        val draggableView = DraggableView(this)
+        draggableView.setDestViewGroup(destinations)
+        draggableView.addViews(views)
     }
 
     override fun viewSelectedPosition(position: Int): Int {
-        when (position) {
-            0 -> {
-//                Toast.makeText(this, "Login with Facebook", Toast.LENGTH_SHORT).show()
-            }
-            1 -> {
-//                Toast.makeText(this, "Login with Google", Toast.LENGTH_SHORT).show()
-            }
-            2 -> {
-//                Toast.makeText(this, "Login with LinkedIn", Toast.LENGTH_SHORT).show()
-            }
-        }
         return position
     }
 }
